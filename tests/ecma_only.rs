7f0eaf9a89bed38ab8db59700c7358fbe58a0ff4
YunfeiHe <i.heyunfei@gmail.com>
2023-05-04 15:39:15 +0800

    Fix

--- tests/ecma_only.rs
+++ tests/ecma_only.rs
@@ -5,4 +5,6 @@ fn supports_look_ahead() {
     // From https://stackoverflow.com/questions/71809435/javascript-regex-into-rust-regex
     let re = Regex::new(r"^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{6,10}$");
     assert!(re.is_ok());
+    let re = Regex::new(r##"#\-te11111s"#t.*\-$/"##);
+    assert!(re.is_ok());
 }
